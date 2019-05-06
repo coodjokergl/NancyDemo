058d8dc5826c42fdf2f4245ce3f523f899aa923a
龚磊 <Gongl01@mingyuanyun.com>
2019-05-06 19:45:14 +0800

    nancy 测试Demo工厂创建
    Signed-off-by: 龚磊 <Gongl01@mingyuanyun.com>

---
+++ NancyDemo/Modules/TestModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NancyDemo.Modules
+{
+    /// <summary>
+    /// 测试模块
+    /// </summary>
+    public class TestModule : Nancy.NancyModule
+    {
+        public TestModule()
+        {
+            Get("/h" , q => @"Hellow word!");
+        }
+    }
+}
